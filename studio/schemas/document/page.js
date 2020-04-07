a81492cf55f70f53ce72123687ea009ad0b2399e
Joris Saïdani <joris@saidani.org>
2020-04-07 03:06:36 +0200

    Update page.js

--- studio/schemas/document/page.js
+++ studio/schemas/document/page.js
@@ -1,23 +1,34 @@
 export default {
-  name: 'page',
-  title: 'Page',
-  type: 'document',
+  name: "page",
+  title: "Page",
+  type: "document",
   fields: [
     {
-      name: 'title',
-      title: 'Titre de la page',
-      type: 'localeString'
+      name: "title",
+      title: "Titre de la page",
+      type: "localeString",
+      validation: (Rule) => Rule.required(),
     },
     {
-      name: 'content',
-      title: 'Contenu',
-      type: 'localeBlockContent'
+      name: "slug",
+      title: "Slug",
+      type: "localeSlug",
+    },
+    {
+      name: "content",
+      title: "Contenu",
+      type: "localeBlockContent",
+    },
+    {
+      name: "createPage",
+      title: "Créer une page dédiée",
+      type: "boolean",
     },
   ],
   preview: {
     select: {
-      title: 'title.br',
-      subtitle: 'title.fr'
-    }
-  }
-}
+      title: "title.br",
+      subtitle: "title.fr",
+    },
+  },
+};
