23aec2d77fe277dbdb735bf2a8cb8da104deb6e4
Joris Saïdani <joris@saidani.org>
2020-04-11 04:55:21 +0200

    upgrade packages + linted

--- web/src/components/seo.js
+++ web/src/components/seo.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import Helmet from "react-helmet"
+import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 import { truncateString } from "../lib/helpers"
 //import { useTranslation } from "react-i18next"
