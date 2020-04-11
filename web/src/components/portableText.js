a8cf43222b1380ed69ff9e0607cc6692abc99939
Joris Saïdani <joris@saidani.org>
2020-04-11 14:18:42 +0200

    href blank for portabletext

--- web/src/components/portableText.js
+++ web/src/components/portableText.js
@@ -7,6 +7,13 @@ const serializers = {
     //authorReference: ({node}) => <span>{node.author.name}</span>,
     //localBlockContent:  ({node}) => {node._rawBody}
   },
+  marks: {
+    link: ({ children, mark }) => (
+      <a href={mark.href} target={mark.blank === false ? "_self" : "_blank"} rel="noopener noreferrer">
+        {children}
+      </a>
+    ),
+  },
 }
 
 const PortableText = ({ blocks }) => (
