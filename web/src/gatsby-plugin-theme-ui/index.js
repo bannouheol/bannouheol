2997adae68e89b5106798ded56468ff654e7a9f0
Joris <joris@saidani.org>
2020-04-19 18:06:39 +0200

    font-display: swap

--- web/src/gatsby-plugin-theme-ui/index.js
+++ web/src/gatsby-plugin-theme-ui/index.js
@@ -191,7 +191,7 @@ export default {
   },
   fonts: {
     sans:
-      'system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
+      'system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji"',
     serif: 'Georgia,Cambria,"Times New Roman",Times,serif',
     mono: 'Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
     monospace: 'Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace',
