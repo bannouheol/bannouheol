03e84ea48e2bfa934941f8fbb031a179dc13a45c
Joris <joris@saidani.org>
2020-04-22 14:00:48 +0200

    padding topmessage

--- src/components/TopMessage.js
+++ src/components/TopMessage.js
@@ -1,17 +1,17 @@
 /** @jsx jsx */
 
-import { jsx, Box } from "theme-ui"
-import { useTranslation } from "react-i18next"
+import { jsx, Box } from 'theme-ui'
+import { useTranslation } from 'react-i18next'
 
 export const TopMessage = () => {
   const {
     t,
     //i18n: { language },
-  } = useTranslation("common")
+  } = useTranslation('common')
 
   return (
-    <Box sx={{ width: "full", textAlign: "center", bg: "grey", color: "white", fontSize: [0, 1, 1, 2] }}>
-      <span sx={{ color: "tomato" }}>COVID-19</span> : {t("covid_message")}
+    <Box sx={{ width: 'full', textAlign: 'center', bg: 'grey', color: 'white', fontSize: [0, 1, 1, 2], p: [1, 0] }}>
+      <span sx={{ color: 'tomato' }}>COVID-19</span> : {t('covid_message')}
     </Box>
   )
 }
