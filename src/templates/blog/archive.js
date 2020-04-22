290ace7896b5ba33e4864294fab174224739e694
Joris <joris@saidani.org>
2020-04-22 18:15:21 +0200

    blog translations

--- src/templates/blog/archive.js
+++ src/templates/blog/archive.js
@@ -1,19 +1,19 @@
 /** @jsx jsx */
-import { jsx, Styled } from "theme-ui"
-import { graphql } from "gatsby"
-import { Layout } from "../../components/Layout"
-import { useTranslation } from "react-i18next"
-import { mapEdgesToNodes, translateRaw } from "../../lib/helpers"
-import { GraphQLErrorList } from "../../components/GraphQLErrorList"
-import { Posts } from "../../components/Blog/Posts"
-import { Link } from "../../components/Link"
-import { Helmet } from "react-helmet"
+import { jsx, Styled } from 'theme-ui'
+import { graphql } from 'gatsby'
+import { Layout } from '../../components/Layout'
+import { useTranslation } from 'react-i18next'
+import { mapEdgesToNodes, translateRaw } from '../../lib/helpers'
+import { GraphQLErrorList } from '../../components/GraphQLErrorList'
+import { Posts } from '../../components/Blog/Posts'
+import { Link } from '../../components/Link'
+import { Helmet } from 'react-helmet'
 
 const ArchivePage = ({ data, errors, ...props }) => {
   const {
     t,
     i18n: { language },
-  } = useTranslation("common")
+  } = useTranslation('common')
   if (errors) {
     return (
       <Layout>
@@ -32,8 +32,8 @@ const ArchivePage = ({ data, errors, ...props }) => {
   const { currentPage, numPages } = props.pageContext
   const isFirst = currentPage === 1
   const isLast = currentPage === numPages
-  const basePath = `/${t("blog:slug")}/${props.pageContext.category ? `${slug}/` : ""}`
-  const prevPage = basePath + (currentPage - 1 === 1 ? "" : (currentPage - 1).toString())
+  const basePath = `/${t('blog:slug')}/${props.pageContext.category ? `${slug}/` : ''}`
+  const prevPage = basePath + (currentPage - 1 === 1 ? '' : (currentPage - 1).toString())
   const nextPage = basePath + (currentPage + 1).toString()
   return (
     <Layout {...props}>
@@ -41,21 +41,19 @@ const ArchivePage = ({ data, errors, ...props }) => {
         {!isFirst && <link rel="prev" href={`/${language}${prevPage}`} />}
         {!isLast && <link rel="next" href={`/${language}${nextPage}`} />}
       </Helmet>
-      <Styled.h1>{(props.pageContext.category && title) || "Blog"}</Styled.h1>
+      <Styled.h1>{(props.pageContext.category && title) || 'Blog'}</Styled.h1>
       {postNodes && postNodes.length > 0 && <Posts nodes={postNodes} />}
       <ul
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "space-between",
-          alignItems: "center",
-          listStyle: "none",
-          padding: 0,
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          alignItems: 'center',
+          listStyle: 'none',
         }}
       >
         {!isFirst && (
-          <Link to={prevPage} rel="prev">
-            ← Page précédente
+          <Link to={prevPage} rel="prev" sx={{ pr: 3 }}>
+            ← {t('blog:previous_page')}
           </Link>
         )}
         {numPages > 1 &&
@@ -63,15 +61,16 @@ const ArchivePage = ({ data, errors, ...props }) => {
             <li
               key={`pagination-number${i + 1}`}
               style={{
-                margin: 0,
+                m: 3,
               }}
             >
               <Link
                 to={`${basePath}${i === 0 ? `` : `/${i + 1}`}`}
-                style={{
-                  textDecoration: "none",
-                  color: i + 1 === currentPage ? "#ffffff" : "",
-                  background: i + 1 === currentPage ? "#007acc" : "",
+                sx={{
+                  textDecoration: 'none',
+                  color: i + 1 === currentPage ? '#ffffff' : '',
+                  background: i + 1 === currentPage ? '#007acc' : '',
+                  p: 2,
                 }}
               >
                 {i + 1}
@@ -80,7 +79,7 @@ const ArchivePage = ({ data, errors, ...props }) => {
           ))}
         {!isLast && (
           <Link to={nextPage} rel="next">
-            Page suivante →
+            {t('blog:next_page')} →
           </Link>
         )}
       </ul>
