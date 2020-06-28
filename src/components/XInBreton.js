234a2247e8b3a93cae055d33d9fa81da72507817
Joris <joris@saidani.org>
2020-06-28 10:50:23 +0200

    exclude dizale from x_in_breton

--- src/components/XInBreton.js
+++ src/components/XInBreton.js
@@ -2,6 +2,8 @@ import { useTranslation } from 'react-i18next'
 
 export const XInBreton = ({ x }) => {
   const { t } = useTranslation('common')
-  const hasBreton = x.toLowerCase().includes('breton') || x.toLowerCase().includes('brezhoneg')
+  const hasBreton = x.toLowerCase().includes('breton')
+    || x.toLowerCase().includes('brezhoneg')
+    || x.toLowerCase().includes('dizale')
   return hasBreton ? x : t('x_in_breton', { x })
 }
