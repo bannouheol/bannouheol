77454d1723729e331d5adcc217bbc2b06e32f91b
Joris <joris@saidani.org>
2020-08-11 13:51:27 +0200

    product-thumbs

--- src/components/Shop/Product.js
+++ src/components/Shop/Product.js
@@ -1,31 +1,31 @@
 /** @jsx jsx */
-import { graphql, useStaticQuery } from 'gatsby'
+import {graphql, useStaticQuery} from 'gatsby'
 import Img from 'gatsby-image'
 import getVideoId from 'get-video-id'
 import path from 'path'
-import { Trans, useTranslation } from 'react-i18next'
-import { FaFacebookSquare, FaPinterestSquare, FaTwitterSquare } from 'react-icons/fa'
+import {Trans, useTranslation} from 'react-i18next'
+import {FaFacebookSquare, FaPinterestSquare, FaTwitterSquare} from 'react-icons/fa'
 import YouTube from 'react-youtube'
-import { SRLWrapper } from 'simple-react-lightbox'
-import { Box, Divider, Flex, Grid, jsx, Styled, Text } from 'theme-ui'
-import { Link } from '../Link'
+import {SRLWrapper} from 'simple-react-lightbox'
+import {Box, Divider, Flex, Grid, jsx, Styled, Text} from 'theme-ui'
+import {Link} from '../Link'
 import PortableText from '../PortableText'
-import { AddToCart } from './AddToCart'
+import {AddToCart} from './AddToCart'
 //import { translateRaw } from '../../lib/helpers'
-import { BookFeature } from './BookFeature'
-import { ProductFeature } from './ProductFeature'
-import { ProfilePreview } from './ProfilePreview'
-import { parseISO, format } from 'date-fns'
-import { XInBreton } from '../XInBreton'
+import {BookFeature} from './BookFeature'
+import {ProductFeature} from './ProductFeature'
+import {ProfilePreview} from './ProfilePreview'
+import {parseISO, format} from 'date-fns'
+import {XInBreton} from '../XInBreton'
 
-export const Product = (product) => {
+export const Product = product => {
   const {
     t,
-    i18n: { language },
+    i18n: {language},
   } = useTranslation('common')
   const {
     site: {
-      siteMetadata: { siteUrl },
+      siteMetadata: {siteUrl},
     },
   } = useStaticQuery(graphql`
     {
@@ -87,13 +87,13 @@ export const Product = (product) => {
     categories,
     traductors,
     bookFeature,
-    productFeature: { inStock, resupplyingDate, ...productFeature },
-    releaseDate,
+    productFeature: {inStock, resupplyingDate, ...productFeature},
+    //releaseDate,
     minimumAge,
     youtubeVideos,
   } = product
 
-  const thumbs = product.images.images.map((i) => i && i.asset && i.asset.fluid)
+  const thumbs = product.images.images.map(i => i && i.asset && i.asset.fluid)
   const image = thumbs.shift()
 
   const resupplying = resupplyingDate ? true : false
@@ -104,7 +104,7 @@ export const Product = (product) => {
   return (
     <article>
       <Grid gap={2} columns={[1, 2, '4fr 6fr 4fr', '4fr 6fr 3fr']} className="boundary-element">
-        <Box sx={{ order: 0 }}>
+        <Box sx={{order: 0}}>
           {image && (
             <Img
               fluid={image}
@@ -115,8 +115,8 @@ export const Product = (product) => {
           )}
           {thumbs && (
             <SRLWrapper>
-              <Grid gap={3} width={[64]}>
-                {thumbs.map((i) => (
+              <Grid gap={3} columns={2} sx={{mt: 3}}>
+                {thumbs.map(i => (
                   <Img key={i && i.src} fluid={i} data-attribute="SRL" />
                 ))}
               </Grid>
@@ -124,8 +124,8 @@ export const Product = (product) => {
           )}
           {youtubeVideos &&
             youtubeVideos.length > 0 &&
-            youtubeVideos.map((video) => {
-              const { id } = getVideoId(video.url)
+            youtubeVideos.map(video => {
+              const {id} = getVideoId(video.url)
               return (
                 id && (
                   <YouTube
@@ -133,7 +133,7 @@ export const Product = (product) => {
                     opts={{
                       width: '100%',
                     }}
-                    sx={{ mt: 3 }}
+                    sx={{mt: 3}}
                   />
                 )
               )
@@ -152,30 +152,30 @@ export const Product = (product) => {
           {language === 'fr' && (
             <Text>
               {t('shop:title_in_brezhoneg')} :{' '}
-              <h2 sx={{ display: 'inline-block', fontSize: 1, m: 0 }}>{product._rawTitle.br}</h2>
+              <h2 sx={{display: 'inline-block', fontSize: 1, m: 0}}>{product._rawTitle.br}</h2>
             </Text>
           )}
           {language === 'br' && (
             <Text>
               {t('shop:original_title')} :{' '}
-              <h2 sx={{ display: 'inline-block', fontSize: 1, m: 0 }}>{product._rawTitle.fr}</h2>
+              <h2 sx={{display: 'inline-block', fontSize: 1, m: 0}}>{product._rawTitle.fr}</h2>
             </Text>
           )}
-          <Box sx={{ lineHeight: 2 }}>
+          <Box sx={{lineHeight: 2}}>
             {t('shop:collection')} :{' '}
-            <h3 sx={{ display: 'inline-block', fontSize: 1, m: 0 }}>
-              <Link sx={{ p: 1, bg: 'light', borderRadius: 8 }} to={`/${collection.slug.current}`}>
+            <h3 sx={{display: 'inline-block', fontSize: 1, m: 0}}>
+              <Link sx={{p: 1, bg: 'light', borderRadius: 8}} to={`/${collection.slug.current}`}>
                 <XInBreton x={collection.title} />
               </Link>
             </h3>{' '}
             {t('shop:categories')} :{' '}
             {categories &&
               categories
-                .map((c) => {
+                .map(c => {
                   c['path'] = path.join('/', c.parent === null ? `` : c.parent.slug.current, c.slug.current)
                   return (
-                    <h4 sx={{ display: 'inline-block', fontSize: 1, m: 0 }}>
-                      <Link key={c.id} to={c.path} sx={{ p: 1, bg: 'light', borderRadius: 8 }}>
+                    <h4 sx={{display: 'inline-block', fontSize: 1, m: 0}}>
+                      <Link key={c.id} to={c.path} sx={{p: 1, bg: 'light', borderRadius: 8}}>
                         <XInBreton x={c.title} />
                       </Link>
                     </h4>
@@ -193,7 +193,7 @@ export const Product = (product) => {
               <Box>
                 {t('shop:traductors')} :{` `}
                 {traductors
-                  .map((t) => <ProfilePreview key={t.id} {...t} showAvatar={false} />)
+                  .map(t => <ProfilePreview key={t.id} {...t} showAvatar={false} />)
                   .reduce((acc, el) => {
                     return acc === null ? [el] : [...acc, ', ', el]
                   }, null)}
@@ -201,15 +201,15 @@ export const Product = (product) => {
             )}
 
             {/*releaseDate && <p>{t('shop:released_on', { date: releaseDate })}</p>*/}
-            {minimumAge && <p>{t('shop:minimum_age', { minimum_age: minimumAge })}</p>}
+            {minimumAge && <p>{t('shop:minimum_age', {minimum_age: minimumAge})}</p>}
           </Box>
         </Box>
-        <Box sx={{ order: [2, 1, 2], mb: [4, 0] }}>
-          <Box sx={{ variant: 'boxes.important' }}>
-            <Grid gap={2} columns={['1fr 2fr', 1]} sx={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Box sx={{order: [2, 1, 2], mb: [4, 0]}}>
+          <Box sx={{variant: 'boxes.important'}}>
+            <Grid gap={2} columns={['1fr 2fr', 1]} sx={{alignItems: 'center', justifyContent: 'center'}}>
               <Box>
-                {(inStock || resupplying) && <div sx={{ mb: 1, fontSize: 3 }}>{productFeature.price.formatted}</div>}
-                <div sx={{ mb: [0, 3], fontSize: 1, color: inStock ? 'secondary' : resupplying ? 'orange' : 'tomato' }}>
+                {(inStock || resupplying) && <div sx={{mb: 1, fontSize: 3}}>{productFeature.price.formatted}</div>}
+                <div sx={{mb: [0, 3], fontSize: 1, color: inStock ? 'secondary' : resupplying ? 'orange' : 'tomato'}}>
                   {inStock && t('shop:in_stock')}
                   {!inStock && resupplying && t('shop:resupplying')}
                   {!inStock && !resupplying && t('shop:out_of_stock')}
@@ -226,55 +226,54 @@ export const Product = (product) => {
                 />
               )}
             </Grid>
-            <Text sx={{ fontSize: 1, mt: 2 }}>
+            <Text sx={{fontSize: 1, mt: 2}}>
               {inStock && (
                 <Trans i18nKey="shop:free_shipping_message">
-                  <span sx={{ color: 'tomato' }}>Livraison offerte</span> à partir de 10€, en 3 jours chez vous
+                  <span sx={{color: 'tomato'}}>Livraison offerte</span> à partir de 10€, en 3 jours chez vous
                 </Trans>
               )}
               {resupplying && (
                 <Trans i18nKey="shop:delivery_on" resupplyingDateFormatted={resupplyingDateFormatted}>
-                  Livré approximativement chez vous le{' '}
-                  <span sx={{ color: 'tomato' }}>{{ resupplyingDateFormatted }}</span>.
+                  Livré approximativement chez vous le <span sx={{color: 'tomato'}}>{{resupplyingDateFormatted}}</span>.
                 </Trans>
               )}
             </Text>
-            <Divider sx={{ my: 3 }} />
-            <Text sx={{ fontSize: 1, mt: 2 }}>
+            <Divider sx={{my: 3}} />
+            <Text sx={{fontSize: 1, mt: 2}}>
               <Trans i18nKey="shop:order_form_message">
                 Vous ne souhaitez pas commander en ligne ? Pas de problème, remplissez ce
-                <a href="/bon_de_commande.pdf" sx={{ color: 'tomato' }} target="_blank" rel="noopener noreferrer">
+                <a href="/bon_de_commande.pdf" sx={{color: 'tomato'}} target="_blank" rel="noopener noreferrer">
                   bon de commande
                 </a>
                 et renvoyez-le nous !
               </Trans>
             </Text>
-            <Divider sx={{ my: 3 }} />
-            <Flex sx={{ alignItems: 'center', color: 'textMuted' }}>
+            <Divider sx={{my: 3}} />
+            <Flex sx={{alignItems: 'center', color: 'textMuted'}}>
               {t('share_on')} :{' '}
               <a
                 href={`https://www.facebook.com/sharer/sharer.php?u=${siteUrl}${productPath}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                sx={{ display: 'flex' }}
+                sx={{display: 'flex'}}
               >
-                <FaFacebookSquare size={24} sx={{ ml: 1, color: 'light' }} />
+                <FaFacebookSquare size={24} sx={{ml: 1, color: 'light'}} />
               </a>{' '}
               <a
                 href={`https://twitter.com/intent/tweet?text=${siteUrl}${productPath}`}
                 target="_blank"
                 rel="noopener noreferrer"
-                sx={{ display: 'flex' }}
+                sx={{display: 'flex'}}
               >
-                <FaTwitterSquare size={24} sx={{ ml: 1, color: 'light' }} />
+                <FaTwitterSquare size={24} sx={{ml: 1, color: 'light'}} />
               </a>{' '}
               <a
                 href={`https://pinterest.com/pin/create/button/?url=${siteUrl}${productPath}&media=&description=`}
                 target="_blank"
                 rel="noopener noreferrer"
-                sx={{ display: 'flex' }}
+                sx={{display: 'flex'}}
               >
-                <FaPinterestSquare size={24} sx={{ ml: 1, color: 'light' }} />
+                <FaPinterestSquare size={24} sx={{ml: 1, color: 'light'}} />
               </a>
             </Flex>
           </Box>
--- src/templates/shop/product.js
+++ src/templates/shop/product.js
@@ -1,36 +1,36 @@
 /** @jsx jsx */
-import { jsx, Box, Styled } from 'theme-ui'
-import { Layout } from '../../components/Layout'
+import {jsx, Box, Styled} from 'theme-ui'
+import {Layout} from '../../components/Layout'
 import SEO from '../../components/SEO'
-import { graphql } from 'gatsby'
-import { GraphQLErrorList } from '../../components/GraphQLErrorList'
-import { Product } from '../../components/Shop/Product'
-import { Products } from '../../components/Shop/Products'
-import { Posts } from '../../components/Blog/Posts'
-import { useTranslation } from 'react-i18next'
-import { mapEdgesToNodes, toPlainText, translateRaw } from '../../lib/helpers'
-import { JsonLd } from 'react-schemaorg'
-import { add, format } from 'date-fns'
-import { XInBreton } from '../../components/XInBreton'
+import {graphql} from 'gatsby'
+import {GraphQLErrorList} from '../../components/GraphQLErrorList'
+import {Product} from '../../components/Shop/Product'
+import {Products} from '../../components/Shop/Products'
+import {Posts} from '../../components/Blog/Posts'
+import {useTranslation} from 'react-i18next'
+import {mapEdgesToNodes, toPlainText, translateRaw} from '../../lib/helpers'
+import {JsonLd} from 'react-schemaorg'
+import {add, format} from 'date-fns'
+import {XInBreton} from '../../components/XInBreton'
 
 const makeSeoTitle = (seoReadyTitle, title, collection) => {
   if (seoReadyTitle) return seoReadyTitle
   if (title) {
     const titles = []
     if (title.toLowerCase().includes(collection.toLowerCase())) {
-      titles.push(XInBreton({ x: title }))
+      titles.push(XInBreton({x: title}))
     } else {
       titles.push(title)
-      titles.push(XInBreton({ x: collection }))
+      titles.push(XInBreton({x: collection}))
     }
     return titles.join(`, `)
   }
 }
 
-const ProductPage = ({ data, errors, ...props }) => {
+const ProductPage = ({data, errors, ...props}) => {
   const {
     t,
-    i18n: { language },
+    i18n: {language},
   } = useTranslation('common')
 
   const {
@@ -38,21 +38,10 @@ const ProductPage = ({ data, errors, ...props }) => {
     sameCollectionProducts,
     blogPosts,
     site: {
-      siteMetadata: { siteUrl },
+      siteMetadata: {siteUrl},
     },
   } = translateRaw(data, language)
-  const {
-    title,
-    seoTitle,
-    images,
-    body,
-    collection,
-    slug,
-    categories,
-    productFeature,
-    releaseDate,
-    reference,
-  } = product
+  const {title, seoTitle, images, body, collection, slug, categories, productFeature, releaseDate, reference} = product
   const sameCollectionProductNodes = mapEdgesToNodes(sameCollectionProducts)
   const blogPostsNodes = mapEdgesToNodes(blogPosts)
 
@@ -65,7 +54,7 @@ const ProductPage = ({ data, errors, ...props }) => {
     categories &&
     categories.length > 0 &&
     categories
-      .map((c) => c.title)
+      .map(c => c.title)
       .reduce((acc, el) => {
         return acc === null ? el : acc + ' > ' + el
       }, null)
@@ -90,7 +79,7 @@ const ProductPage = ({ data, errors, ...props }) => {
             offers: {
               '@type': 'Offer',
               priceCurrency: 'EUR',
-              priceValidUntil: format(add(new Date(), { years: 1 }), 'yyyy-MM-dd'),
+              priceValidUntil: format(add(new Date(), {years: 1}), 'yyyy-MM-dd'),
               price: productFeature.price.value,
               itemCondition: 'https://schema.org/NewCondition',
               availability: inStock ? 'http://schema.org/InStock' : 'https://schema.org/OutOfStock',
@@ -108,8 +97,8 @@ const ProductPage = ({ data, errors, ...props }) => {
 
       {product && <Product {...product} />}
       {sameCollectionProductNodes && sameCollectionProductNodes.length > 0 && (
-        <Box mt={3}>
-          <Styled.h4>{t('shop:in_collection', { collection_title: product.collection.title })}</Styled.h4>
+        <Box mt={[3, 5]}>
+          <Styled.h4>{t('shop:in_collection', {collection_title: product.collection.title})}</Styled.h4>
           <Products nodes={sameCollectionProductNodes} />
         </Box>
       )}
@@ -125,20 +114,17 @@ const ProductPage = ({ data, errors, ...props }) => {
 
 export const query = graphql`
   query Product($product: String, $collection: String) {
-    product: sanityProduct(id: { eq: $product }) {
+    product: sanityProduct(id: {eq: $product}) {
       ...productFields
     }
     sameCollectionProducts: allSanityProduct(
       filter: {
-        id: { ne: $product },
-        collection: { id: { eq: $collection } }
-        slug: {
-          fr: {current: {ne: null}},
-          br: {current: {ne: null}}
-        },
-        _id: {regex: "/^(?!draft)/"} 
+        id: {ne: $product}
+        collection: {id: {eq: $collection}}
+        slug: {fr: {current: {ne: null}}, br: {current: {ne: null}}}
+        _id: {regex: "/^(?!draft)/"}
       }
-      sort: { order: [DESC, DESC], fields: [defaultProductVariant___inStock, releaseDate] }
+      sort: {order: [DESC, DESC], fields: [defaultProductVariant___inStock, releaseDate]}
       limit: 6
     ) {
       edges {
@@ -148,8 +134,8 @@ export const query = graphql`
       }
     }
     blogPosts: allSanityBlogPost(
-      sort: { fields: [publishedAt], order: DESC }
-      filter: { products: { elemMatch: { id: { eq: $product } } } }
+      sort: {fields: [publishedAt], order: DESC}
+      filter: {products: {elemMatch: {id: {eq: $product}}}}
     ) {
       edges {
         node {
